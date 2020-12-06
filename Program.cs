6763de45b5b5eb3f336cb44408838d601c8562e2
st4n1sl4w <stanislav.mushenyk@gmail.com>
2020-12-06 16:45:23 +0200

    my first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This work! Version 1.0");
+            Console.ReadLine();
+        }
+    }
+}
